7becb02ef4dceb0683a0a9be7f97a599098d6816
Guilherme Araújo <guilhermegpop@gmail.com>
2026-05-18 01:20:10 -0300

    feat: Adicionando api de salvar e resgatar usuarios

---
+++ back-end-fintech/src/main/java/com/fiap/fintech/repository/UsersRepository.java
@@ -0,0 +1,16 @@
+package com.fiap.fintech.repository;
+
+
+import com.fiap.fintech.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, String> {
+
+    Optional<Users> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
---
+++ back-end-fintech/src/main/java/com/fiap/fintech/service/UsersService.java
@@ -0,0 +1,38 @@
+package com.fiap.fintech.service;
+
+
+import com.fiap.fintech.model.Users;
+import com.fiap.fintech.repository.UsersRepository;
+import jakarta.transaction.Transactional;
+import org.apache.catalina.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsersService {
+
+    private final UsersRepository usersRepository;
+
+    public UsersService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    public List<Users> getAllUsers() {
+        return usersRepository.findAll();
+    }
+
+    public Optional<Users> getUserById(String userid) {
+        return usersRepository.findById(userid);
+    }
+
+    @Transactional
+    public Users saveUser(Users user) {
+        if (usersRepository.existsByEmail(user.getEmail())) {
+            throw new IllegalArgumentException("Email already exists");
+        }
+        return usersRepository.save(user);
+    }
+
+}
