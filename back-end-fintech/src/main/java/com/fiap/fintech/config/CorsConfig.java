1d803c6e96c9ed5fb85611db70ae0e26aa922795
Guilherme Araújo <53230801+GuilhermeAGoncalves@users.noreply.github.com>
2026-05-26 23:11:07 -0300

    Update CORS configuration to allow all origins and methods

--- back-end-fintech/src/main/java/com/fiap/fintech/config/CorsConfig.java
+++ back-end-fintech/src/main/java/com/fiap/fintech/config/CorsConfig.java
@@ -13,18 +13,11 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-<<<<<<< Updated upstream
                 registry.addMapping("/**")
                         .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD")
                         .allowedHeaders("*")
                         .allowCredentials(true);
-=======
-                registry.addMapping("/api/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
->>>>>>> Stashed changes
             }
         };
     }
